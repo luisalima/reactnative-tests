a465450c7a3a0f8e8dbe3464649583326e28dd0c
Rui Pereira <ruiafonsopereira@gmail.com>
2016-02-13 11:12:44 +0000

    Fix code indentation

--- WeatherProject/index.ios.js
+++ WeatherProject/index.ios.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  */
 import React, {
-	AppRegistry,
+  AppRegistry,
 } from 'react-native';
 
 
--- WeatherProject/src/Forecast.js
+++ WeatherProject/src/Forecast.js
@@ -1,44 +1,44 @@
 'use strict';
 import React, {
-		Component,
-		StyleSheet,
-		Text,
-		View
+  Component,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
 
 class Forecast extends Component {
-		render() {
-				return(
-								<View>
-								<Text style={styles.bigText}>
-								{this.props.main}
-						</Text>
-								<Text style={styles.mainText}>
-								Current conditions:
-						{this.props.description}
-						</Text>
-								<Text style={styles.bigText}>
-								{this.props.temp} F
-						</Text>
-								</View>
-				);
-		}
+  render() {
+    return(
+      <View>
+        <Text style={styles.bigText}>
+          {this.props.main}
+        </Text>
+        <Text style={styles.mainText}>
+          Current conditions:
+          {this.props.description}
+        </Text>
+        <Text style={styles.bigText}>
+          {this.props.temp} F
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-		bigText: {
-				flex: 2,
-				fontSize: 20,
-				textAlign: 'center',
-				margin: 10,
-				color: '#FFF',
-		},
-		mainText: {
-				flex: 1,
-				fontSize: 16,
-				textAlign: 'center',
-				color: '#FFF',
-		},
+  bigText: {
+    flex: 2,
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFF',
+  },
+  mainText: {
+    flex: 1,
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#FFF',
+  },
 });
 
 // IMPORTANT it seems like this can be the only export of the module. Try putting {} around Forecast...
