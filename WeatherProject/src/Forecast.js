6be09235b55d71f7f2fa6f463323a69f8eac5a26
Rui Pereira <ruiafonsopereira@gmail.com>
2016-02-13 22:47:13 +0000

    Fetch current weather data for one location

--- WeatherProject/src/Forecast.js
+++ WeatherProject/src/Forecast.js
@@ -1,44 +1,101 @@
 'use strict';
+
 import React, {
+  Animated,
   Component,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
 
+import API from 'WeatherProject/src/Api';
+
 class Forecast extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: 'Porto',
+      country: 'Portugal',
+      description: 'Clear',
+      temperature: 21,
+      searchedCity: 'Porto'
+    };
+  }
+
+  _fetchWeather() {
+    API.fetchWeatherByCityName(this.state.searchedCity).then((response => {
+      let city = response.list[0];
+
+      this.setState({
+        temperature: city.main.temp,
+        city: city.name,
+        country: city.sys.country,
+        description: city.weather[0].description
+      });
+    }));
+  }
+
   render() {
-    return(
-      <View>
-        <Text style={styles.bigText}>
-          {this.props.main}
-        </Text>
-        <Text style={styles.mainText}>
-          Current conditions:
-          {this.props.description}
-        </Text>
-        <Text style={styles.bigText}>
-          {this.props.temp} F
-        </Text>
-      </View>
+    return (
+      <Animated.View style={styles.container}>
+        <View style={[styles.animatedContainer]}>
+          <Text style={styles.description}>
+            {this.state.description}
+          </Text>
+
+          <Text style={styles.temperature}>
+            {Math.round(this.state.temperature) + 'ºC'}
+          </Text>
+
+          <TextInput
+            style={styles.input}
+            clearButtonMode={'always'}
+            clearTextOnFocus={true}
+            returnKeyType={'search'}
+            onChangeText={(text) => this.setState({searchedCity: text})}
+            onSubmitEditing={() => this._fetchWeather()}/>
+        </View>
+      </Animated.View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  bigText: {
-    flex: 2,
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#FFF',
-  },
-  mainText: {
+  container: {
     flex: 1,
-    fontSize: 16,
-    textAlign: 'center',
-    color: '#FFF',
+    alignItems: 'stretch',
+    justifyContent: 'center'
   },
+  animatedContainer: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  temperature: {
+    fontSize: 90,
+    fontWeight: '100',
+    margin: 0
+  },
+  location: {
+    fontSize: 14,
+    fontWeight: '100',
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 34,
+    fontWeight: '500'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#666',
+    height: 40,
+    marginVertical: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    borderRadius: 5
+  }
 });
 
 // IMPORTANT it seems like this can be the only export of the module. Try putting {} around Forecast...
