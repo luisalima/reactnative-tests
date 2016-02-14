f18a137509e507a49e71e157652f196ee8e8f23a
Rui Pereira <ruiafonsopereira@gmail.com>
2016-02-14 11:03:45 +0000

    Add basic NavigatorIOS component

--- WeatherProject/index.ios.js
+++ WeatherProject/index.ios.js
@@ -6,8 +6,41 @@
 
 import React, {
   AppRegistry,
+  Component,
+  NavigatorIOS,
+  StyleSheet
 } from 'react-native';
 
-import WeatherProject from 'WeatherProject/src/Forecast';
+import ForecastScreen from 'WeatherProject/src/Forecast';
+
+class WeatherProject extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: 'Porto'
+    };
+  }
+
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: this.state.city,
+          component: ForecastScreen
+        }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  }
+});
 
 AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
