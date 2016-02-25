fc636f117ac0cfc518af53f19bf0ee213df175b6
Rui Pereira <ruiafonsopereira@gmail.com>
2016-02-25 21:43:16 +0000

    Fix bug when parsing time

--- WeatherProject/src/Utils.js
+++ WeatherProject/src/Utils.js
@@ -6,7 +6,8 @@ let Utils = {
    * Convert the specified time in unit time format to the `hh:mm` format.
    */
   formatTime: function(unix_time) {
-    let date = new Date(unix_time);
+    // The argument must be in miliseconds instead of seconds
+    let date = new Date(unix_time * 1000);
     let h    = date.getHours();
     let m    = date.getMinutes();
 
